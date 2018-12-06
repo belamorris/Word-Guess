470f297549d0d6e322632e36c63bfb993cd4e328
Robert M <belamorris@gmail.com>
2018-12-05 18:06:12 -0700

    just need to end a sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 //create variables
 
-var $placeholders = document.getElementById("placeholders");
-var $lettersGuessed = document.getElementById("lettersGuessed");
-var $guessesRemaining = document.getElementById("remaining");
-var $wins = document.getElementById("wins");
-var $losses = document.getElementById("losses");
+var placeholders = document.getElementById("placeholders");
+var lettersGuessed = document.getElementById("lettersGuessed");
+var guessesRemaining = document.getElementById("remaining");
+var jwins = document.getElementById("wins");
+var jlosses = document.getElementById("losses");
 
 var wordBank = ["Dude", "Walter", "Lebowski", "Bunny", "Donnie"];
 var wins = 0;
@@ -35,9 +35,9 @@ function newGame (){
           placeholdersArr.push("_");
       }
   }
-  $guessesRemaining.textContent = guessesLeft;
-  $placeholders.textContent = placeholdersArr.join(" ");
-  $lettersGuessed.textContent = incorectLettersBank.join(" ");
+  guessesRemaining.textContent = guessesLeft;
+  placeholders.textContent = placeholdersArr.join(" ");
+  lettersGuessed.textContent = incorectLettersBank.join(" ");
 }
 
 
@@ -53,42 +53,32 @@ function newGame (){
             if (pickedword[i].toLowerCase() === letter.toLowerCase()) {
                 placeholdersArr[i] = pickedword[i];
             }
+            
+          
            
         }
 
-       $placeholders.textContent = placeholdersArr.join(" "); 
+       placeholders.textContent = placeholdersArr.join(" "); 
        checkIncorrect(letter);
+    
 
       }
-      else {
-         if (gameRunning === false){
-            alert ("press any key to start");
-         } 
-         else {
-             alert ("guess a new letter");
-         }
-      }
-    
+     
     }
 
 
 //function to check if letter was correct
-function checkIncorrect(letter){
-
-
-
-     if (
-        placeholdersArr.indexOf(letter.toLowerCase) < 0 
-       
-     ){
+function checkIncorrect (letter){
+    if (placeholdersArr.indexOf(letter.toLowerCase())=== -1 ){
+        guessesLeft--;
         incorectLettersBank.push(letter);
-        guessesLeft --;
-         $lettersGuessed.textContent = incorectLettersBank.join(" ");
-         $guessesRemaining.textContent = guessesLeft;
-    
-        }
-        checkLost();
+        lettersGuessed.textContent = incorectLettersBank.join(" ");
+        guessesRemaining.textContent = guessesLeft;
     }
+    checkLost ();
+}
+
+   
     
 
 //check if lost
@@ -96,7 +86,7 @@ function checkLost(){
     if (guessesLeft === 0){
         losses++;
         gameRunning= false;
-        $losses.textContent = losses;
+        jlosses.textContent = losses;
     }
     checkWin();
 }
@@ -107,19 +97,19 @@ function checkWin() {
     if (pickedword.toLowerCase() === placeholdersArr.join("").toLowerCase()){
        wins++;
        gameRunning = false;
-       $wins.textContent = wins;
+       jwins.textContent = wins;
     }
 }
 
 //event to start new game
 
 document.onkeyup = function (event) {
-    // If finished, dump one keystroke and reset
+    
     if (gameRunning === false) {
         newGame();
         gameRunning = true;
     } else {
-        // Check to make sure a (65) - z (90) was pressed.
+       
         if (event.keyCode >= 65 && event.keyCode <= 90) {
             letterGuess(event.key.toLowerCase());
         }
